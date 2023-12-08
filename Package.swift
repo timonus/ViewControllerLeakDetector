1726fdf63ddb3e670a4d4d08df7918d38d1b630c
Eric Jensen <jensen39@gmail.com>
2023-12-08 15:49:50 -0500

    Add a Swift Package description file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "UIViewControllerLeakDetector",
+    platforms: [.iOS(.v13), .tvOS(.v13)],
+    products: [
+        .library(
+            name: "UIViewControllerLeakDetector",
+            targets: ["UIViewControllerLeakDetector"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "UIViewControllerLeakDetector",
+            path: ".",
+            sources: ["UIViewController+TJLeakDetection.m"],
+            publicHeadersPath: "."
+        )
+    ]
+)
